a08b95d46085267aef043068541567a9d5a0e6b0
Elrey Belmonti and Megan Olson <elrey+megan@>
2017-12-11 08:52:33 -0800

    Create seed file and add destination table.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+class Seed
+  def self.begin
+    seed = Seed.new
+    seed.generate_destinations
+  end
+
+  def generate_destinations
+    20.times do |i|
+      city =
+      destination = Destination.create!(
+        city: Faker::Address.city,
+        country: Faker::Address.country
+      )
+      puts "Destination #{i}: City is #{destination.city} and country is #{destination.country}."
+    end
+  end
+end
+
+Seed.begin
