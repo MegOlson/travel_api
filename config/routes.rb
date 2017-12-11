a3aa7f552e45720710ad9ce4c1a0c3fb3272d734
Elrey Belmonti and Megan Olson <elrey+megan@>
2017-12-11 14:27:01 -0800

    Edit routes for revies.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :destinations do
-    resources :reviews
+    resources :reviews, only: [:index, :create]
   end
+
+  resources :reviews, only: [:show, :update, :destroy]
 end
