84700a0e54174643d76f7977c0234ac6d2782d5e
Elrey Belmonti and Megan Olson <elrey+megan@>
2017-12-12 09:23:35 -0800

    Add versioning to api controllers.

---
+++ app/controllers/api/v1/destinations_controller.rb
@@ -0,0 +1,50 @@
+module Api
+  module V1
+    class DestinationsController < ApplicationController
+
+      def index
+        @destinations = Destination.all
+        country = params[:country]
+        if country
+          @destinations = Destination.search(country)
+        end
+        json_response(@destinations)
+      end
+
+      def show
+        @destination = Destination.find(params[:id])
+        json_response(@destination)
+      end
+
+      def create
+        @destination = Destination.create!(destination_params)
+        json_response(@destination, :created)
+      end
+
+      def update
+        @destination = Destination.find(params[:id])
+        if @destination.update!(destination_params)
+          render status: 200, json: {
+            message: "Your destination has been updated successfully!"
+          }
+        end
+      end
+
+      def destroy
+        @destination = Destination.find(params[:id])
+        if @destination.destroy!
+          render status: 200, json: {
+            message: "Your destination has been deleted successfully!"
+          }
+        end
+      end
+
+    private
+
+      def destination_params
+        params.permit(:city, :country)
+      end
+
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :destinations do
-    resources :reviews
+  namespace :api do
+    namespace :v1 do
+      resources :destinations do
+        resources :reviews
+      end
+    end
   end
 end
