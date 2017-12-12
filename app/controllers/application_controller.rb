7562c5ab506ff0b5f91c7ce7b2f8b849943a3a9a
Elrey Belmonti and Megan Olson <elrey+megan@>
2017-12-12 09:07:33 -0800

    Add basic authentication for everything except index.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Basic::ControllerMethods
+  http_basic_authenticate_with name: "dhh", password: "password", except: :index
+
+  def index
+    render plain: "Everyone can see me!"
+  end
+
+  def edit
+    render plain: "I'm only accessible if you know the super secret password."
+  end
+
   include Response
 
   rescue_from ActiveRecord::RecordNotFound do |exception|
